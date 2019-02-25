1e1571044dc7eebf7f87f86296cb8088eb562905
Michal Slavka <michalslavka@gmail.com>
2019-02-25 11:53:44 +0100

    Removed debug info
    	modified:   cmcts.cpp

--- mcts/cmcts.cpp
+++ mcts/cmcts.cpp
@@ -111,17 +111,14 @@ Cmcts::search(void)
 	while ((value = is_end()) == 0){
 		/* new expanded node */
 		if (current->nodeN == -1){
-			//std::cout << "\tleaf node" <<std::endl;
 
 			/* simulation */
 			if (predict != nullptr){
-				std::cout << "not heur" << std::endl;
 				gsl_ran_dirichlet(r, SIZE, alpha, dir);
 				prediction_t = predict(nn_input(), data);
 				value = -prediction_t[0].cast<float>();
 				current->set_prior(prediction_t[1].cast<py::array_t<float>>(), dir);
 			}else{
-				std::cout << "not predict" << std::endl;
 #ifdef HEUR
 				value = rollout();
 				current->set_prior(hboard);
@@ -135,7 +132,6 @@ Cmcts::search(void)
 		nodes.push(current);
 		/* select new node, if it's leaf then expand */
 		action = current->select(board);
-		std::cout << "\tnext: "<< action <<std::endl;
 		current = current->next_node(action);
 		board[player*SIZE + action] = 1;
 #ifdef HEUR
@@ -227,24 +223,6 @@ Cmcts::is_end()
 	for (int j = 0; j < SHAPE; j++){
 		// sum of first five horizontal elements for paler 1
 		// I hope that compiler will rollout this loop
-		std::cout << std::endl << j << std::endl;
-		std::cout << "v0 ";
-		for (int k = 0; k < SHAPE; k++)
-			std::cout << board[k*SHAPE+j] << ", ";
-		std::cout << std::endl;
-		std::cout << "v1 ";
-		for (int k = 0; k < SHAPE; k++)
-			std::cout << board[k*SHAPE+j+SIZE] << ", ";
-		std::cout << std::endl;
-		std::cout << "h0 ";
-		for (int k = 0; k < SHAPE; k++)
-			std::cout << board[j*SHAPE+k] << ", ";
-		std::cout << std::endl;
-		std::cout << "h1 ";
-		for (int k = 0; k < SHAPE; k++)
-			std::cout << board[j*SHAPE+k+SIZE] << ", ";
-		std::cout << std::endl;
-
 		h0 = h1 = v0 = v1 = 0;
 		for (int i = 0; i < 5; i++){
 			h0 += board[j*SHAPE+i];
@@ -252,8 +230,6 @@ Cmcts::is_end()
 			v0 += board[i*SHAPE+j];
 			v1 += board[i*SHAPE+j+SIZE];
 		}
-			std::cout << "j: " << j << ", i: 0-4" << std::endl;
-			std::cout << "h0: " << h0 << ", h1: " << h1 << ", v0: " << v0 << ", v1: " << v1 << std::endl;
 		if (h0 == 5 || v0 == 5){
 			return player == 0 ? 1. : -1.;
 		}
@@ -268,8 +244,6 @@ Cmcts::is_end()
 			v0 = v0 - board[i*SHAPE+j]      + board[(i+5)*SHAPE+j];
 			v1 = v1 - board[i*SHAPE+j+SIZE] + board[(i+5)*SHAPE+j+SIZE];
 
-				std::cout << "j: " << j << ", i: " << i << std::endl;
-				std::cout << "h0: " << h0 << ", h1: " << h1 << ", v0: " << v0 << ", v1: " << v1 << std::endl;
 			if (h0 == 5 || v0 == 5){
 				return player == 0 ? 1. : -1.;
 			}
@@ -289,24 +263,6 @@ Cmcts::is_end()
 		   sum of first elements in diagonal taken from j-th index
 		   the same is done for flipped board
 		*/
-		std::cout << std::endl << j << std::endl;
-		std::cout << "lr0 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[k*SHAPE+k+j] << ", ";
-		std::cout << std::endl;
-		std::cout << "lr1 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[k*SHAPE+k+j+SIZE] << ", ";
-		std::cout << std::endl;
-		std::cout << "rl0 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[k*SHAPE+SHAPE-1-k-j] << ", ";
-		std::cout << std::endl;
-		std::cout << "rl1 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[k*SHAPE+SHAPE-1-k-j+SIZE] << ", ";
-		std::cout << std::endl;
-
 		lr0 = lr1 = rl0 = rl1 = 0;
 		for (int i = 0; i < 5; i++){
 			lr0 += board[i*SHAPE+i+j];
@@ -314,8 +270,6 @@ Cmcts::is_end()
 			rl0 += board[i*SHAPE+SHAPE-1-i-j];
 			rl1 += board[i*SHAPE+SHAPE-1-i-j+SIZE];
 		}
-		std::cout << "j: " << j << ", i: 0-4" << std::endl;
-		std::cout << "lr0: " << lr0 << ", lr1: " << lr1 << ", rl0: " << rl0  << ", rl1: " << rl1 << std::endl;
 		if (lr0 == 5 || rl0 == 5){
 			return player == 0 ? 1. : -1.;
 		}
@@ -329,8 +283,6 @@ Cmcts::is_end()
 			rl0 = rl0 - board[i*SHAPE+SHAPE-1-i-j]      + board[(i+5)*SHAPE+SHAPE-1-i-j-5];
 			rl1 = rl1 - board[i*SHAPE+SHAPE-1-i-j+SIZE] + board[(i+5)*SHAPE+SHAPE-1-i-j-5+SIZE];
 
-			std::cout << "j: " << j << ", i: " << i << std::endl;
-			std::cout << "lr0: " << lr0 << ", lr1: " << lr1 << ", rl0: " << rl0  << ", rl1: " << rl1 << std::endl;
 			if (lr0 == 5 || rl0 == 5){
 				return player == 0 ? 1. : -1.;
 			}
@@ -345,24 +297,6 @@ Cmcts::is_end()
 		   the same as above but moving in y direction
 		   starts from one to exclude main diagonal which was computed above
 		*/
-		std::cout << std::endl << j << std::endl;
-		std::cout << "lr0 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[(k+j)*SHAPE+k] << ", ";
-		std::cout << std::endl;
-		std::cout << "lr1 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[(k+j)*SHAPE+k+SIZE] << ", ";
-		std::cout << std::endl;
-		std::cout << "rl0 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[(SHAPE-1-k-j)*SHAPE+k] << ", ";
-		std::cout << std::endl;
-		std::cout << "rl1 ";
-		for (int k = 0; k < SHAPE-j; k++)
-			std::cout << board[(SHAPE-1-k-j)*SHAPE+k+SIZE] << ", ";
-		std::cout << std::endl;
-
 		lr0 = lr1 = rl0 = rl1 = 0;
 		for (int i = 0; i < 5; i++){
 			lr0 += board[(i+j)*SHAPE+i];
@@ -370,8 +304,6 @@ Cmcts::is_end()
 			rl0 += board[(SHAPE-1-i-j)*SHAPE+i];
 			rl1 += board[(SHAPE-1-i-j)*SHAPE+i+SIZE];
 		}
-		std::cout << "j: " << j << ", i: 0-4" << std::endl;
-		std::cout << "lr0: " << lr0 << ", lr1: " << lr1 << ", rl0: " << rl0  << ", rl1: " << rl1 << std::endl;
 
 		if (lr0 == 5 || rl0 == 5){
 			return player == 0 ? 1. : -1.;
@@ -385,9 +317,6 @@ Cmcts::is_end()
 			lr1 = lr1 - board[(i+j)*SHAPE+i+SIZE] + board[(i+j+5)*SHAPE+i+5+SIZE];
 			rl0 = rl0 - board[(SHAPE-1-i-j)*SHAPE+i]      + board[(SHAPE-1-i-j-5)*SHAPE+i+5];
 			rl1 = rl1 - board[(SHAPE-1-i-j)*SHAPE+i+SIZE] + board[(SHAPE-1-i-j-5)*SHAPE+i+5+SIZE];
-
-			std::cout << "j: " << j << ", i: " << i << std::endl;
-			std::cout << "lr0: " << lr0 << ", lr1: " << lr1 << ", rl0: " << rl0  << ", rl1: " << rl1 << std::endl;
 
 			if (lr0 == 5 || rl0 == 5){
 				return player == 0 ? 1. : -1.;
@@ -407,61 +336,33 @@ Cmcts::is_end()
 float
 Cmcts::rollout()
 {
+	int action;
+	float v = 0;
+	double max = 0;
+	double h = 0;
+	/* save original state */
 	std::array<double,2*SIZE> start_hboard = hboard;
 	Board start_board = board;
 	int start_player = player;
 	int start_cnt = move_cnt;
-	size_t action;
-	float v = 0;
-	std::vector<size_t> moves;
-	double max = 0;
-	double h = 0;
 
 	while ((v = is_end()) == 0){
-		max = -1;
+		/* choose action */
 		for (int a = 0; a < SIZE; a++){
 			if (board[a] == 0 and board[SIZE+a] == 0){
 				h = hboard[a]+hboard[SIZE+a];
 				if (max < h){
-		std::cout << "rollout max h: "<< h << ", x: " << board[a] << ", o: "<< board[SIZE+a] << std::endl;
 					max = h;
 					action = a;
 				}
 			}
 		}
-		std::cout << "\nrollout moves: " << move_cnt << std::endl;
-		std::cout << "rollout is_end: " << v << std::endl;
-		std::cout << "rollout board x: " << board[action] << ", o: "<< board[SIZE+action] << std::endl;
-		std::cout << "rollout act: " << action << std::endl;
-		std::cout << "rollout player: " << player << std::endl;
 		if (max == -1){
-			std::cout << "No more good moves" <<std::endl;
 			break;
 		}
+		max = -1;
+
 		board[player*SIZE + action] = 1;
-		std::cout << "rollout board x: " << board[action] << ", o: "<< board[SIZE+action] << std::endl;
-	for (size_t i=0; i<SHAPE; i++){
-		for (size_t j=0; j<SHAPE; j++)
-			if (board[i*SHAPE+j] == 1 && board[i*SHAPE+j+SIZE] == 1)
-				if (i*SHAPE+j == action)
-					std::cout << "@ ";
-				else
-					std::cout << "@ ";
-			else if (board[i*SHAPE+j] == 1)
-				if (i*SHAPE+j == action)
-					std::cout << "X ";
-				else
-					std::cout << "x ";
-			else if (board[i*SHAPE+j+SIZE] == 1)
-				if (i*SHAPE+j+SIZE == action)
-					std::cout << "O ";
-				else
-					std::cout << "o ";
-			else
-				std::cout << "_ ";
-		std::cout << std::endl;
-	}
-		moves.push_back(action);
 		update(action);
 		player = player ? 0 : 1;
 		move_cnt++;
@@ -469,52 +370,8 @@ Cmcts::rollout()
 			break;
 	}
 
-
-		std::cout << "\nrollout moves: " << move_cnt << std::endl;
-		std::cout << "rollout is_end: " << v << std::endl;
-		std::cout << "rollout board x: " << board[action] << ", o: "<< board[SIZE+action] << std::endl;
-		std::cout << "rollout player: " << player << std::endl;
-		std::cout << "rollout board x: " << board[action] << ", o: "<< board[SIZE+action] << std::endl;
-	for (size_t i=0; i<SHAPE; i++){
-		for (size_t j=0; j<SHAPE; j++)
-			if (board[i*SHAPE+j] == 1 && board[i*SHAPE+j+SIZE] == 1)
-				if (i*SHAPE+j == action)
-					std::cout << "@ ";
-				else
-					std::cout << "@ ";
-			else if (board[i*SHAPE+j] == 1)
-				if (i*SHAPE+j == action)
-					std::cout << "X ";
-				else
-					std::cout << "x ";
-			else if (board[i*SHAPE+j+SIZE] == 1)
-				if (i*SHAPE+j+SIZE == action)
-					std::cout << "O ";
-				else
-					std::cout << "o ";
-			else
-				std::cout << "_ ";
-		std::cout << std::endl;
-	}
-	//std::cout << "rollout board: " << std::endl;
-	//for (int i=0; i<SHAPE; i++){
-	//	for (int j=0; j<SHAPE; j++)
-	//			std::cout << hboard[i*SHAPE+j] << " ";
-	//	std::cout << std::endl;
-	//}
-	//std::cout << "rollout board: " << std::endl;
-	//for (int i=0; i<SHAPE; i++){
-	//	for (int j=0; j<SHAPE; j++)
-	//		std::cout << hboard[i*SHAPE+j+SIZE] << " ";
-	//	std::cout << std::endl;
-	//}
-
-	std::sort(moves.begin(), moves.end());
-	for (size_t move : moves)
-		std::cout << move << ", ";
-	std::cout << std::endl;
-
-
+	v = start_player == player ? v : -v;
+	/* restore original state */
 	hboard = start_hboard;
 	move_cnt = start_cnt;
 	board = start_board;
@@ -646,15 +503,10 @@ Cmcts::update(int action)
 	rbound = r;
 
 	tmp = x > y ? action-(SHAPE-1)*(y+1) : action-(SHAPE-1)*(x+1);
-	//std::cout << "lr \\" << std::endl;
-	//std::cout << "\tright max: " << tmp << std::endl;
 	while (tmp < lbound && !board[op*SIZE+lbound]) lbound -= (SHAPE-1);
 	tmp = x > y ? action+(SHAPE-x+1)*(SHAPE-1) : action+(SHAPE-y+1)*(SHAPE-1);
 	while (tmp > rbound && !board[op*SIZE+rbound]) rbound += (SHAPE-1);
 
-	//std::cout << "\tleft max: " << tmp << std::endl;
-	//std::cout << "\tright bound: " << rbound << std::endl;
-	//std::cout << "\tleft bound:  " << lbound << std::endl;
 
 	if (rbound/SHAPE-lbound/SHAPE > 5){
 		while (lbound < l && board[player*SIZE+l]) l -= (SHAPE-1);
@@ -662,18 +514,13 @@ Cmcts::update(int action)
 		// y coordinate of bottom(r) minus y coordinate of top(l)
 		// vertical distance between r and l
 		reward = r/SHAPE-l/SHAPE-1;
-		//std::cout << "\tr: " << r << std::endl;
-		//std::cout << "\tl: " << l << std::endl;
-		//std::cout << "\treward: " << reward << std::endl;
 		if (lbound != l && (hboard[l]||hboard[SIZE+l])){
 			hboard[player*SIZE+l] += std::pow(4, reward);
-			//std::cout << "\t-lreward: " << action/SHAPE-l/SHAPE-1 << std::endl;
 //			if (action/SHAPE-l/SHAPE-1 != 0)
 //				hboard[player*SIZE+l] += std::pow(4, action/SHAPE-l/SHAPE-1);
 		}
 		if (rbound != r && (hboard[r]||hboard[SIZE+r])){
 			hboard[player*SIZE+r] += std::pow(4, reward);
-			//std::cout << "\t-rreward: " << r/SHAPE-action/SHAPE-1 << std::endl;
 //			if (r/SHAPE-action/SHAPE-1 != 0)
 //				hboard[player*SIZE+r] += std::pow(4, r/SHAPE-action/SHAPE-1);
 		}
