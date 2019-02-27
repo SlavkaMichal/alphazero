94263334afcc28518c5c30eb2700f1b97ce04c7b
Michal Slavka <michalslavka@gmail.com>
2019-02-27 20:39:55 +0100

    [config] changed managing configureation

---
+++ config.py
@@ -0,0 +1,24 @@
+# IF YOU CHANGE ANY OF THE VARIABLES BELLOW
+# YOU MUST RUN sudo python setup.py install
+
+# version
+MAJOR = 0
+MINOR = 1
+# dimensions of board
+SHAPE = 13
+
+# number of positions on board
+SIZE = SHAPE*SHAPE
+
+# exploration constant
+CPUCT = 5
+
+# initialisation of random number generator
+RANDOM = False
+
+# compile with support for heuristic and rollout
+HEUR = True
+
+# debug output
+DEBUG = False
+
