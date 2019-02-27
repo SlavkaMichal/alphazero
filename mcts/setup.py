0970b64f3272db39de9d035a67b357b190803165
Michal Slavka <michalslavka@gmail.com>
2019-02-27 20:39:55 +0100

    [config] changed managing configureation

--- mcts/setup.py
+++ mcts/setup.py
@@ -1,15 +1,28 @@
 from setuptools import setup, Extension
 from setuptools.command import build_ext
 from pybind11 import get_include
-from const import SIZE, SHAPE, CPUCT
+from os.path import dirname
+import sys
 
-__version__ = '0.0.1'
+sys.path.append(dirname(sys.path[0]))
+from config import SIZE, SHAPE, CPUCT, RANDOM, HEUR, DEBUG
+from config import MINOR, MAJOR
+
+__version__ = "{:02d}.{:02d}".format(MAJOR, MINOR)
+
+extra_compile_args = ['-std=c++14', '-Werror', '-fvisibility=hidden', ]
+if HEUR:
+    extra_compile_args.append('-DHEUR')
+if RANDOM:
+    extra_compile_args.append('-DRAND')
+if DEBUG:
+    extra_compile_args.append('-DDBG')
 
 cmcts = Extension('cmcts',
         ['module.cpp','cmcts.cpp','node.cpp'],
         include_dirs = [get_include(), get_include(True)],
         language = 'c++',
-        extra_compile_args = ['-std=c++14', '-Werror', '-fvisibility=hidden', '-DHEUR'],
+        extra_compile_args = extra_compile_args,
         extra_link_args = ['-lgsl', '-lgslcblas', '-lm'],
         define_macros=[
             ('SIZE', SIZE),
