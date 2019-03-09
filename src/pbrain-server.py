39dcdd2e5f7672b82e41e2310cac9b2c2cf8029a
Michal Slavka <michalslavka@gmail.com>
2019-03-09 13:24:37 +0100

    [pbrain] piskvork evaluation

---
+++ src/pbrain-server.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+import sys
+sys.path.append('..')
+from config import SHAPE, SIZE, SIMS, CMCTS_PATH
+import site
+site.addsitedir(CMCTS_PATH)
+
+import argparse
+import server as cmds
+from server import Server
+import cmcts
+
+HOST = '127.0.0.1'
+PORT = 27015
+args = 0
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--ip', default=HOST, type=str)
+    parser.add_argument('-p', '--port', default=PORT, type=int)
+
+    return parser.parse_args()
+
+
+def main():
+    global args
+    args = get_args()
+
+    print("DBG:Play: starting server")
+    server = Server(args.ip, args.port)
+    mcts = cmcts.mcts()
+
+    while True:
+        print("DBG:Play: waiting for connection")
+        server.connect()
+
+        while True:
+            cmd, move = server.get_cmd()
+
+            if cmd == cmds.OP_MOVE:
+                print("Opponents move")
+                sys.stdout.flush()
+                mcts.make_move(x = move[0], y = move[1])
+            elif cmd == cmds.MAKE_MOVE:
+                print("My move")
+                sys.stdout.flush()
+                mcts.simulate(SIMS)
+                pi = mcts.get_prob()
+                mcts.print_node([])
+                move = np.argmax(pi)
+                mcts.make_move(move)
+                server.make_move((move%SHAPE,move//SHAPE))
+            elif cmd == cmds.LOAD_MOVE:
+                print("Loading move")
+                sys.stdout.flush()
+                raise NotImplementedError
+                #game.set_player()
+                #game.make_move(move)
+                #game.set_player(1)
+            elif cmd == cmds.INITIALIZE:
+                print("Initializing")
+                sys.stdout.flush()
+                print(move)
+                #assert move == SHAPE
+                mcts = cmcts.mcts()
+            elif cmd == cmds.END:
+                print("End")
+                sys.stdout.flush()
+                break
+            else:
+                print('Error: unknow command: '+cmd)
+                sys.stdout.flush()
+                raise NotImplementedError
+
+
+    return
+
+if __name__ == "__main__":
+    main()
