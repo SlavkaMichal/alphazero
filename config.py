5805bb87630ccc84165ee4c800240db9d5be749b
Michal Slavka <michalslavka@gmail.com>
2019-03-24 14:47:23 +0100

    [config] adds configuration options

--- config.py
+++ config.py
@@ -2,14 +2,30 @@ import sys
 import os
 
 ##############################################################################
+#                                   general                                  #
+##############################################################################
+# neural network to be used
+MODEL_MODULE = "model"
+MODEL_CLASS = "simplerNN"
+
+# path to neural network parameters
+PARAM_PATH = "{}/model".format(os.path.dirname(os.path.realpath(__file__)))
+
+# path to data
+DATA_PATH = "{}/data".format(os.path.dirname(os.path.realpath(__file__)))
+
+# file containing path to best parameters
+PARAM_BEST = "{}/.param_best".format(os.path.dirname(os.path.realpath(__file__)))
+
+##############################################################################
 #                                     mcts                                   #
 ##############################################################################
 # number of simulations performed by MCTS
-SIMS = 3000
+SIMS = 800
 
 # how many training examples should be generated
 # optimally tens of thousands
-TRAIN_SAMPLES = 1
+TRAIN_SAMPLES = 8000
 
 # parameter influencing generating of dirichlet noise
 # x = avg_game_length = SHAPE*2
@@ -26,10 +42,16 @@ CPUCT = 4.
 # number of epochs
 EPOCHS = 2
 
+# training learning rate
+LR = 0.001
+
+# batch size
+BATCH_SIZE = 16
+
 # number of generations to train from
 # size of window is increasing by one every n generations
 # (starting window, max window, n)
-WINDOW = (4,2,20)
+WINDOW = (4,20,2)
 
 ##############################################################################
 #     IF YOU CHANGE ANY OF THE VARIABLES BELLOW YOU MUST RUN ./install.sh    #
@@ -40,17 +62,17 @@ PREFIX = "{}/.local".format(os.environ["HOME"])
 
 # test if pytorch is available
 INSTALL_PYTORCH = False
-try:
-    import pytorch
-except ModlueNotFoundError:
-    INSTALL_PYTORCH = True
+#try:
+#    import pytorch
+#except ModlueNotFoundError:
+#    INSTALL_PYTORCH = True
 
 # test if pybind11 is available
 INSTALL_PYBIND11 = False
-try:
-    import pybind11
-except ModlueNotFoundError:
-    INSTALL_PYBIND11 = True
+#try:
+#    import pybind11
+#except ModlueNotFoundError:
+#    INSTALL_PYBIND11 = True
 
 # cmcts site path
 LOCAL_SITE_PATH = "{}/lib/python{}.{}/site-packages".format(PREFIX,sys.version_info[0],sys.version_info[1])
@@ -58,7 +80,7 @@ PYVERSION  = True if sys.version_info[0] >= 3 and sys.version_info[1] > 4 else F
 
 # version
 MAJOR = 0
-MINOR = 3
+MINOR = 4
 
 # dimensions of board
 SHAPE = 13
